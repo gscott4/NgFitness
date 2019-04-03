5f4a2c5eb714ada525661a8a1f8c205e4c1469b4
gscott4 <grayson.scott@outlook.com>
2019-04-03 16:36:03 -0400

    Added current training logic and dialog options

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { StopTrainingComponent } from './stop-training.component';
+
 
 @Component({
   selector: 'app-current-training',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentTrainingComponent implements OnInit {
 
-  constructor() { }
+  progress = 0;
+  timer;
+
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+
+    this.timer = setInterval(() => {
+      this.progress = this.progress + 5;
+      if(this.progress >= 100) {
+        clearInterval(this.timer);
+      }
+    }, 1000);
+  }
+
+  onStop() {
+    clearInterval(this.timer);
+    const dialogReg = this.dialog.open(StopTrainingComponent, {data: {
+      progress: this.progress
+    }});
+
+    dialogReg.afterClosed().subscribe(result => {
+      console.log(result);
+    })
   }
 
 }
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingComponent implements OnInit {
 
+  
+  ongoingTraining = true;
+
   constructor() { }
 
   ngOnInit() {
